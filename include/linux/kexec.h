d1ecec4ca69c70b17bb666c6c94595bd4d405ae2
Sourabh Jain <sourabhjain@linux.ibm.com>
2023-10-24 01:47:10 +0530

    crash: make CPU and Memory hotplug support reporting flexible
    Architectures' specific functions `arch_crash_hotplug_cpu_support()` and
    `arch_crash_hotplug_memory_support()` advertise the kernel's capability
    to update the kdump image on CPU and Memory hotplug events to userspace
    via the sysfs interface. These architecture-specific functions need to
    access various attributes of the `kexec_crash_image` object to determine
    whether the kernel can update the kdump image and advertise this
    information to userspace accordingly.
    
    As the architecture-specific code is not exposed to the APIs required to
    acquire the lock for accessing the `kexec_crash_image` object, it calls
    a generic function, `crash_check_update_elfcorehdr()`, to determine
    whether the kernel can update the kdump image or not.
    
    The lack of access to the `kexec_crash_image` object in
    architecture-specific code restricts architectures from performing
    additional architecture-specific checks required to determine if the
    kdump image is updatable or not. For instance, on PowerPC, the kernel
    can update the kdump image only if both the elfcorehdr and FDT are
    marked as updatable for the `kexec_load` system call.
    
    So define two generic functions, `crash_hotplug_cpu_support()` and
    `crash_hotplug_memory_support()`, which are called when userspace
    attempts to read the crash CPU and Memory hotplug support via the sysfs
    interface. These functions take the necessary locks needed to access the
    `kexec_crash_image` object and then forward it to the
    architecture-specific handler to do the rest.
    
    Signed-off-by: Sourabh Jain <sourabhjain@linux.ibm.com>
    Cc: Akhil Raj <lf32.dev@gmail.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Aneesh Kumar K.V <aneesh.kumar@kernel.org>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Borislav Petkov (AMD) <bp@alien8.de>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Eric DeVolder <eric.devolder@oracle.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Hari Bathini <hbathini@linux.ibm.com>
    Cc: Laurent Dufour <laurent.dufour@fr.ibm.com>
    Cc: Mahesh Salgaonkar <mahesh@linux.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Mimi Zohar <zohar@linux.ibm.com>
    Cc: Naveen N Rao <naveen@kernel.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: kexec@lists.infradead.org
    Cc: x86@kernel.org

--- include/linux/kexec.h
+++ include/linux/kexec.h
@@ -486,15 +486,16 @@ static inline void arch_kexec_pre_free_pages(void *vaddr, unsigned int pages) { 
 static inline void arch_crash_handle_hotplug_event(struct kimage *image, void *arg) { }
 #endif
 
-int crash_check_update_elfcorehdr(void);
-
-#ifndef crash_hotplug_cpu_support
-static inline int crash_hotplug_cpu_support(void) { return 0; }
+#ifndef arch_crash_hotplug_cpu_support
+static inline int arch_crash_hotplug_cpu_support(struct kimage *image) { return 0; }
 #endif
 
-#ifndef crash_hotplug_memory_support
-static inline int crash_hotplug_memory_support(void) { return 0; }
+#ifndef arch_crash_hotplug_memory_support
+static inline int arch_crash_hotplug_memory_support(struct kimage *image) { return 0; }
 #endif
+
+extern int crash_hotplug_cpu_support(void);
+extern int crash_hotplug_memory_support(void);
 
 #ifndef crash_get_elfcorehdr_size
 static inline unsigned int crash_get_elfcorehdr_size(void) { return 0; }
