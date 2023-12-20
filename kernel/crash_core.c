7133f0568685d8d1e3a4507cacdccd21f7b15b4e
Sourabh Jain <sourabhjain@linux.ibm.com>
2023-12-20 09:16:29 +0530

    crash: add a new kexec flag for hotplug support
    Commit a72bbec70da2 ("crash: hotplug support for kexec_load()")
    introduced a new kexec flag, `KEXEC_UPDATE_ELFCOREHDR`. Kexec tool uses
    this flag to indicate to the kernel that it is safe to modify the
    elfcorehdr of the kdump image loaded using the kexec_load system call.
    
    However, it is possible that architectures may need to update kexec
    segments other then elfcorehdr. For example, FDT (Flatten Device Tree)
    on PowerPC. Introducing a new kexec flag for every new kexec segment
    may not be a good solution. Hence, a generic kexec flag bit,
    `KEXEC_CRASH_HOTPLUG_SUPPORT`, is introduced to share the CPU/Memory
    hotplug support intent between the kexec tool and the kernel for the
    kexec_load system call.
    
    Now, if the kexec tool sends KEXEC_CRASH_HOTPLUG_SUPPORT kexec flag to
    the kernel, it indicates to the kernel that all the required kexec
    segment is skipped from SHA calculation and it is safe to update kdump
    image loaded using the kexec_load syscall.
    
    While loading the kdump image using the kexec_load syscall, the
    @update_elfcorehdr member of struct kimage is set if the kexec tool
    sends the KEXEC_UPDATE_ELFCOREHDR kexec flag. This member is later used
    to determine whether it is safe to update elfcorehdr on hotplug events.
    However, with the introduction of the KEXEC_CRASH_HOTPLUG_SUPPORT kexec
    flag, the kexec tool could mark all the required kexec segments on an
    architecture as safe to update. So rename the @update_elfcorehdr to
    @hotplug_support. If @hotplug_support is set, the kernel can safely
    update all the required kexec segments of the kdump image during
    CPU/Memory hotplug events.
    
    Introduce an architecture-specific function to process kexec flags for
    determining hotplug support. Set the @hotplug_support member of struct
    kimage for both kexec_load and kexec_file_load system calls. This
    simplifies kernel checks to identify hotplug support for the currently
    loaded kdump image by just examining the value of @hotplug_support.
    
    Signed-off-by: Sourabh Jain <sourabhjain@linux.ibm.com>
    Cc: Akhil Raj <lf32.dev@gmail.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Aneesh Kumar K.V <aneesh.kumar@kernel.org>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Borislav Petkov (AMD) <bp@alien8.de>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Eric DeVolder <eric.devolder@oracle.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Hari Bathini <hbathini@linux.ibm.com>
    Cc: Laurent Dufour <laurent.dufour@fr.ibm.com>
    Cc: Mahesh Salgaonkar <mahesh@linux.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Mimi Zohar <zohar@linux.ibm.com>
    Cc: Naveen N Rao <naveen@kernel.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: kexec@lists.infradead.org
    Cc: x86@kernel.org

--- kernel/crash_core.c
+++ kernel/crash_core.c
@@ -886,7 +886,7 @@ static DEFINE_MUTEX(__crash_hotplug_lock);
  * It reflects the kernel's ability/permission to update the crash
  * elfcorehdr directly.
  */
-int crash_check_update_elfcorehdr(void)
+int crash_check_hotplug_support(void)
 {
 	int rc = 0;
 
@@ -898,10 +898,7 @@ int crash_check_update_elfcorehdr(void)
 		return 0;
 	}
 	if (kexec_crash_image) {
-		if (kexec_crash_image->file_mode)
-			rc = 1;
-		else
-			rc = kexec_crash_image->update_elfcorehdr;
+		rc = kexec_crash_image->hotplug_support;
 	}
 	/* Release lock now that update complete */
 	kexec_unlock();
@@ -942,8 +939,8 @@ static void crash_handle_hotplug_event(unsigned int hp_action, unsigned int cpu,
 
 	image = kexec_crash_image;
 
-	/* Check that updating elfcorehdr is permitted */
-	if (!(image->file_mode || image->update_elfcorehdr))
+	/* Check that kexec segments update is permitted */
+	if (!image->hotplug_support)
 		goto out;
 
 	if (hp_action == KEXEC_CRASH_HP_ADD_CPU ||
