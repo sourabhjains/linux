a31df9dd51a84a43c3e97632248b052c053c6921
Sourabh Jain <sourabhjain@linux.ibm.com>
2024-10-28 07:51:31 +0530

    powerpc/crash: enable high, low crashkernel reservation
    Signed-off-by: Sourabh Jain <sourabhjain@linux.ibm.com>

--- arch/powerpc/include/asm/crash_reserve.h
+++ arch/powerpc/include/asm/crash_reserve.h
@@ -5,4 +5,10 @@
 /* crash kernel regions are Page size agliged */
 #define CRASH_ALIGN             PAGE_SIZE
 
+#define DEFAULT_CRASH_KERNEL_LOW_SIZE SZ_256M
+
+#define CRASH_ADDR_LOW_MAX	ppc64_rma_size
+#define CRASH_ADDR_HIGH_MAX	memblock_end_of_DRAM()
+
+
 #endif /* _ASM_POWERPC_CRASH_RESERVE_H */
