4523e917c39dcb7ef0abf184f59ee90e4e0694a9
Sourabh Jain <sourabhjain@linux.ibm.com>
2024-11-07 12:27:29 +0530

    powerpc/crash: make low and high crashkernel reservation adjacent
    Signed-off-by: Sourabh Jain <sourabhjain@linux.ibm.com>

--- arch/powerpc/include/asm/crash_reserve.h
+++ arch/powerpc/include/asm/crash_reserve.h
@@ -5,10 +5,10 @@
 /* crash kernel regions are Page size agliged */
 #define CRASH_ALIGN		PAGE_SIZE
 
-#define DEFAULT_CRASH_KERNEL_LOW_SIZE SZ_1G
+#define DEFAULT_CRASH_KERNEL_LOW_SIZE SZ_128M
 
-#define CRASH_ADDR_LOW_MAX	get_crash_base(0) + DEFAULT_CRASH_KERNEL_LOW_SIZE
-#define CRASH_ADDR_HIGH_MAX	memblock_end_of_DRAM()
+#define CRASH_ADDR_LOW_MAX	SZ_512M + SZ_256M + SZ_128M + SZ_64M + SZ_16M + SZ_1M
+#define CRASH_ADDR_HIGH_MAX	CRASH_ADDR_LOW_MAX + SZ_4G
 
 
 #endif /* _ASM_POWERPC_CRASH_RESERVE_H */
