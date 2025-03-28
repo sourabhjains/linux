dd7a09be431978e944099742c437cf8940e017b4
Sourabh Jain <sourabhjain@linux.ibm.com>
2025-03-28 17:27:28 +0530

    powerpc: load segments till crashk_res.end
    Signed-off-by: Sourabh Jain <sourabhjain@linux.ibm.com>

--- arch/powerpc/kexec/elf_64.c
+++ arch/powerpc/kexec/elf_64.c
@@ -52,9 +52,7 @@ static void *elf64_load(struct kimage *image, char *kernel_buf,
 	if (IS_ENABLED(CONFIG_CRASH_DUMP) && image->type == KEXEC_TYPE_CRASH) {
 		/* min & max buffer values for kdump case */
 		kbuf.buf_min = pbuf.buf_min = crashk_res.start;
-		kbuf.buf_max = pbuf.buf_max =
-				((crashk_res.end < ppc64_rma_size) ?
-				 crashk_res.end : (ppc64_rma_size - 1));
+		kbuf.buf_max = pbuf.buf_max = crashk_res.end;
 	}
 
 	ret = kexec_elf_load(image, &ehdr, &elf_info, &kbuf, &kernel_load_addr);
