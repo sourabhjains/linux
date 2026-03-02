c548dafbd79dbbce27f3111c53fa2241f65a739b
Jinjie Ruan <ruanjinjie@huawei.com>
2026-03-02 11:53:15 +0800

    riscv: kexec: Add support for crashkernel CMA reservation
    Commit 35c18f2933c5 ("Add a new optional ",cma" suffix to the
    crashkernel= command line option") and commit ab475510e042 ("kdump:
    implement reserve_crashkernel_cma") added CMA support for kdump
    crashkernel reservation. This allows the kernel to dynamically allocate
    contiguous memory for crash dumping when needed, rather than permanently
    reserving a fixed region at boot time.
    
    So extend crashkernel CMA reservation support to riscv. The following
    changes are made to enable CMA reservation:
    
    - Parse and obtain the CMA reservation size along with other crashkernel
      parameters.
    - Call reserve_crashkernel_cma() to allocate the CMA region for kdump.
    - Include the CMA-reserved ranges for kdump kernel to use, which was
      already done in of_kexec_alloc_and_setup_fdt().
    - Exclude the CMA-reserved ranges from the crash kernel memory to
      prevent them from being exported through /proc/vmcore, which was
      already done in the crash core.
    
    Update kernel-parameters.txt to document CMA support for crashkernel on
    riscv architecture.
    
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Acked-by: Paul Walmsley <pjw@kernel.org> # arch/riscv
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- arch/riscv/kernel/machine_kexec_file.c
+++ arch/riscv/kernel/machine_kexec_file.c
@@ -46,7 +46,7 @@ static int get_nr_ram_ranges_callback(struct resource *res, void *arg)
 
 unsigned int arch_get_system_nr_ranges(void)
 {
-	unsigned int nr_ranges = 1; /* For exclusion of crashkernel region */
+	unsigned int nr_ranges = 1 + crashk_cma_cnt; /* For exclusion of crashkernel region */
 
 	walk_system_ram_res(0, -1, &nr_ranges, get_nr_ram_ranges_callback);
 
