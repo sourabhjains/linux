0c5a05dafa9e623f25eb7f76fa7422a81d36227b
Shivang Upadhyay <shivangu@linux.ibm.com>
2026-08-28 15:19:42 +0530

    ppc/fadump: collect dump when CPU_STATE_DATA is less than reserved
    During Fadump in Qemu VM, when maxcpus value is set to more than current
    cpus, following failure is observed.
    
        [    0.045806] [      T1] rtas fadump: Dump taken by platform is
            incomplete (0)
    
    This is because the CPU_STATE_DATA is allocated for maxcpus, while the
    data is only filled for current cpus. As per current implementation
    of Fadump, dumped_bytes and source_len for a region have to match,
    Which is failing for qemu's case, Even tough the dumped bytes are
    reported correctly for only the current cpus. After this /proc/vmcore
    generatition also fails.
    
    Allowing dumped_bytes to be lesser than or equal to allocated length, for
    CPU_STATE_DATA Fadump region.
    
    Reported-by: Anushree Mathur <anushree.mathur@linux.vnet.ibm.com>
    Closes: https://lore.kernel.org/all/5e66daf4-3f55-4044-94a5-4f50bb040849@linux.ibm.com/T/#u
    Signed-off-by: Shivang Upadhyay <shivangu@linux.ibm.com>

--- arch/powerpc/platforms/pseries/rtas-fadump.c
+++ arch/powerpc/platforms/pseries/rtas-fadump.c
@@ -459,6 +459,8 @@ static int __init rtas_fadump_process(struct fw_dump *fadump_conf)
 	/* Check if the dump data is valid. */
 	for (int i = 0; i < be16_to_cpu(fdm_active->header.dump_num_sections); i++) {
 		int type = be16_to_cpu(fdm_active->rgn[i].source_data_type);
+		uint64_t bytes_dumped = be64_to_cpu(fdm_active->rgn[i].bytes_dumped);
+		uint64_t source_len = be64_to_cpu(fdm_active->rgn[i].source_len);
 		int rc = 0;
 
 		switch (type) {
@@ -469,10 +471,20 @@ static int __init rtas_fadump_process(struct fw_dump *fadump_conf)
 				pr_err("Dump taken by platform is not valid (%d)\n", i);
 				rc = -EINVAL;
 			}
-			if (fdm_active->rgn[i].bytes_dumped != fdm_active->rgn[i].source_len) {
+
+			/*
+			 * Make sure that dump is collected for entire region.
+			 * CPU_STATE_DATA region is allowed to dump less than allocated space.
+			 */
+			if (!(bytes_dumped == source_len ||
+			(type == RTAS_FADUMP_CPU_STATE_DATA && bytes_dumped <= source_len))) {
+
 				pr_err("Dump taken by platform is incomplete (%d)\n", i);
+				pr_debug("type -> %d, bytes_dumped -> %llx, source_len -> %llx\n",
+					 type, bytes_dumped, source_len);
 				rc = -EINVAL;
 			}
+
 			if (rc) {
 				pr_warn("Region type: %u src addr: 0x%llx dest addr: 0x%llx\n",
 					be16_to_cpu(fdm_active->rgn[i].source_data_type),
@@ -482,7 +494,7 @@ static int __init rtas_fadump_process(struct fw_dump *fadump_conf)
 			}
 			break;
 		case RTAS_FADUMP_PARAM_AREA:
-			if (fdm_active->rgn[i].bytes_dumped != fdm_active->rgn[i].source_len ||
+			if (bytes_dumped != source_len ||
 			    fdm_active->rgn[i].error_flags != 0) {
 				pr_warn("Failed to process additional parameters! Proceeding anyway..\n");
 				fadump_conf->param_area = 0;
